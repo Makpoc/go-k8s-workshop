8629c6b76728b0af159c4f69283f520bb244c2a8
bnedkov <bnedkov@axway.com>
2018-10-06 11:30:21 +0300

    Add hello router

--- cmd/go-k8s-workshop/main.go
+++ cmd/go-k8s-workshop/main.go
@@ -3,10 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	log.Print("Hello, world")
 
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", hello)
+
+	log.Fatal(http.ListenAndServe(":8888", router))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
 }
