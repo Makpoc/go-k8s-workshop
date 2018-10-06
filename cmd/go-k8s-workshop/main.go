96c938a5afd37cf12146a21bed941fc759ef96e8
bnedkov <bnedkov@axway.com>
2018-10-06 12:07:34 +0300

    Get ports from env

--- cmd/go-k8s-workshop/main.go
+++ cmd/go-k8s-workshop/main.go
@@ -3,28 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/makpoc/go-k8s-workshop/internal/diagnostics"
 )
 
 func main() {
-	log.Print("Hello, world")
+	log.Print("Starting service...")
+
+	blPort := os.Getenv("PORT")
+	if blPort == "" {
+		log.Fatal("PORT not provided")
+	}
+	diagPort := os.Getenv("DIAG_PORT")
+	if diagPort == "" {
+		log.Fatal("DIAG_PORT not provided")
+	}
 
 	router := mux.NewRouter()
-
 	router.HandleFunc("/", hello)
 
 	go func() {
-		err := http.ListenAndServe(":8888", router)
+		err := http.ListenAndServe(":"+blPort, router)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
 
 	diagnostics := diagnostics.NewDiagnostics()
-
-	err := http.ListenAndServe(":9999", diagnostics)
+	err := http.ListenAndServe(":"+diagPort, diagnostics)
 	if err != nil {
 		log.Fatal(err)
 	}
